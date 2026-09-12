163ba047e11d645f3f13fb2152ac51cbacddd702
Ayesha Siddiqa Khan <179512742+Ayesha-Siddiqa-khan@users.noreply.github.com>
2026-09-12 23:27:01 +0500

    fix(infra): allow inbound port 6443 and NodePorts from 0.0.0.0/0 for GitHub Actions and external access

--- terraform/security-groups.tf
+++ terraform/security-groups.tf
@@ -19,7 +19,7 @@ resource "aws_security_group" "ec2_consolidated" {
     from_port   = 6443
     to_port     = 6443
     protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/16"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
@@ -83,7 +83,7 @@ resource "aws_security_group" "ec2_consolidated" {
     from_port   = 30000
     to_port     = 32767
     protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/16"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
